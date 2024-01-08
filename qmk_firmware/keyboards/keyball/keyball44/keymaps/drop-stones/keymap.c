6be8e202ea66256e1fa14737ed537a9d4949cec5
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-08 20:07:55 +0900

    feat(keymaps): change default keymaps

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/drop-stones/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/drop-stones/keymap.c
@@ -19,37 +19,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
+#include "quantum_keycodes.h"
 #include "keyball44.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3,KC_LNG1),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    _______  , KC_Q         , KC_W         , KC_E          , KC_R          , KC_T          ,                   KC_Y           , KC_U            , KC_I            , KC_O           , KC_P            , _______ ,
+    _______  , LCTL_T(KC_A) , KC_S         , KC_D          , KC_F          , KC_G          ,                   KC_H           , KC_J            , KC_K            , KC_L           , RCTL_T(KC_SCLN) , _______ ,
+    _______  , LSFT_T(KC_Z) , LWIN_T(KC_X) , LALT_T(KC_C)  , KC_V          , KC_B          ,                   KC_N           , KC_M            , RALT_T(KC_COMM) , RWIN_T(KC_DOT) , RSFT_T(KC_SLSH) , _______ ,
+               KC_LWIN      , KC_LALT      , LT(1, KC_TAB) , LT(2, KC_ENT) , LT(3, KC_DEL) ,                   LT(2, KC_BSPC) , LT(1, KC_SPACE) , _______         , _______        , KC_ESC
   ),
 
   [1] = LAYOUT_universal(
-    _______  ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    _______  ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______  ,  S(KC_1) , S(KC_2)    , S(KC_3)    , S(KC_4)    , S(KC_5)   ,                    S(KC_6)    , S(KC_7)    , S(KC_8)    , S(KC_9)   , S(KC_0)    , _______ ,
+    _______  ,  _______ , S(KC_BSLS) , S(KC_LBRC) , S(KC_RBRC) , S(KC_EQL) ,                    KC_MINS    , S(KC_QUOT) , KC_QUOT    , S(KC_GRV) , S(KC_SCLN) , _______ ,
+    _______  ,  _______ , KC_BSLS    , KC_LBRC    , KC_RBRC    , KC_EQL    ,                    S(KC_MINS) , KC_GRV     , S(KC_COMM) , S(KC_DOT) , S(KC_SLSH) , _______ ,
+                _______ , _______    , _______    , _______    , _______   ,                    _______    , _______    , _______    , _______   , _______
   ),
 
   [2] = LAYOUT_universal(
-    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+    _______  ,  KC_1   , KC_2    , KC_3    , KC_4    , KC_5    ,                    KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ ,
+    _______  , _______ , _______ , _______ , _______ , _______ ,                    KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_QUOT , _______ ,
+    _______  , _______ , _______ , _______ , _______ , _______ ,                    KC_HOME , KC_PGDN , KC_PGUP , KC_END  , _______ , _______ ,
+               _______ , _______ , _______ , _______ , _______ ,                    _______ , _______ , _______ , _______ , _______
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT    , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    _______  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                    KC_F6    , KC_F7    , KC_F8   , KC_F9   , KC_F10  , _______ ,
+    _______  , KC_F11  , KC_F12  , KC_F13  , KC_F14  , KC_F15  ,                    KC_F16   , KC_F17   , KC_F18  , KC_F19  , KC_F20  , _______ ,
+    _______  , _______ , _______ , _______ , _______ , _______ ,                    CPI_D100 , CPI_I100 , KC_VOLD , KC_VOLU , KC_MUTE , _______ ,
+               _______ , _______ , _______ , _______ , _______ ,                    _______  , _______  , _______ , _______ , _______
   ),
 };
 // clang-format on
