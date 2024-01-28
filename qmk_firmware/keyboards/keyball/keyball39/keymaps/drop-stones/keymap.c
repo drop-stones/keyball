3ecc8b492bb075ea42c02d6452bf6dfb849555f2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-28 19:58:20 +0900

    feat(permissive_hold): set permissive hold per key

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/keymap.c
@@ -115,3 +115,24 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     }
 }
 #endif
+
+#ifdef PERMISSIVE_HOLD_PER_KEY
+#    include "action_tapping.h"
+#    include <stdint.h>
+
+// Dual-role keys perform hold action if a dual-role key and another key are pressed within tapping term
+// and another key is released first
+// https://docs.qmk.fm/#/tap_hold?id=tapping-term
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1, KC_TAB): // Layer keys perform a hold action preferentially
+        case LT(2, KC_ENTER):
+        case LT(3, KC_DEL):
+        case LT(1, KC_SPACE):
+        case LT(2, KC_BSPC):
+            return true;
+        default:
+            return false;
+    }
+}
+#endif
