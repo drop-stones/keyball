bf36f31c64c76e1e99f094e485bc83032dc6e86c
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-06-03 18:31:23 +0900

    feat(quick_tap): set quick tappings per key

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/config.h
@@ -44,6 +44,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
+#define QUICK_TAP_TERM 120
+#define QUICK_TAP_TERM_PER_KEY
+
 // Automatic Mouse Layer
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6 // override layer settings
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
--- qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/keymap.c
@@ -194,6 +194,28 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 }
 #endif
 
+#ifdef QUICK_TAP_TERM_PER_KEY
+#    include "action_tapping.h"
+#    include <stdint.h>
+
+// When the user holds a key after tapping it, the tapping function is repeated by default, rather than activating the hold function.
+// https://docs.qmk.fm/tap_hold#quick-tap-term
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LT(1, KC_TAB): // Tab/Space may be tapped quickly but frequently typed so disable them
+    case LT(1, KC_SPACE):
+    case LT(2, KC_ENTER): // Enter must not be tapped quickly
+      return 0;
+    case LT(2, KC_BSPC):  // Delete/BackSpace must be tapped quickly
+    case LT(3, KC_DEL):
+      return QUICK_TAP_TERM_PER_KEY - 20;
+    default:
+      return QUICK_TAP_TERM;
+  }
+}
+
+#endif
+
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 // https://docs.qmk.fm/#/feature_pointing_device?id=pointing-device-auto-mouse
 void pointing_device_init_user(void) {
