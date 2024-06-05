109e383e5b9008ba19fb4c71a56359a2dc31bdab
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-06-05 20:18:50 +0900

    feat(keymaps): converts to {LCTRL/RALT}+Space when {LCTRL/RALT}+Enter

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/drop-stones/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Number/Arrow Layer
   [2] = LAYOUT_universal(
-    KC_1   , KC_2    , KC_3    , KC_4    , KC_5    ,                      KC_6    , KC_7    , KC_8    , KC_9    , KC_0    ,
+    KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                      KC_6    , KC_7    , KC_8    , KC_9    , KC_0    ,
     _______ , _______ , _______ , _______ , _______ ,                      KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_QUOT ,
     _______ , _______ , _______ , _______ , _______ ,                      KC_HOME , KC_PGDN , KC_PGUP , KC_END  , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ , _______ , _______
@@ -111,8 +111,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(2, KC_ENTER):
             if (record->tap.count && record->event.pressed) {
-                // CTRL/SHIFT/ALT/GUI + Enter => CTRL/SHIFT/ALT/GUI + Space
-                if (get_mods() & MOD_MASK_CSAG) {
+                // LCTRL/RALT + Enter => LCTRL/RALT + Space
+                if (get_mods() & (MOD_BIT(KC_LCTL) | MOD_BIT(KC_RALT))) {
                     tap_code16(KC_SPACE);
                     return false;
                 }
